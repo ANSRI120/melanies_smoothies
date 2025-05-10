42af98087c0f017799c7a7399a57d6d26aadf076
ANSRI120 <ansri.storage001@gmail.com>
2025-05-11 02:25:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,5 @@ if ing_list:
 #new section to display smoohyfroot nutritional input
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) 
