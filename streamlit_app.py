1a75726fc09ff355dc25e848c40e557d86a18565
ANSRI120 <ansri.storage001@gmail.com>
2025-05-11 04:01:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import requests
 import requests
 import pandas
 
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -14,55 +13,37 @@ st.write(
   """
 )
 
-### Adding a select box 
-
-#option = st.selectbox(
-#    "Which fruit would you like have smoothie made of?",
-#    ("Banana", "Apple", "Mango", "Strawberry", "Guava", "Peach", "Orange", 
-#        "Grapes", "Dragonfruit"),
-#)
-
-#st.write("You have selected:", option)
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The Name on Smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
-## add list of fruits from table
-#session = get_active_session()
+
 session = cnx.session()
 my_dataframe = session.table ('smoothies.public.fruit_options').select(col('FRUIT_NAME'), col('SEARCH_ON'))
-#st.dataframe (data = my_dataframe, use_container_width = True)
-#st.stop
 
-##convert snowpark dataframe to pandas
 pd_df = my_dataframe.to_pandas()
 st.dataframe (pd_df)
-#st.stop()
-##multiselect
-ing_list = st.multiselect('Choose up to 5 Fruits: ', my_dataframe, max_selections= 5)
+
+ing_list = st.multiselect('Choose up to 5 Fruits: ',
+                          my_dataframe, 
+                          max_selections= 5)
 
 if ing_list:
     ing_string = ''
     for i in ing_list:
         ing_string += i + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
         st.subheader(i + "  Nitrution Information ")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + search_on)
-       #st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) 
-        #st.subheader('fruit_chosen' + "  Nitrution Information ")
-    #st.write(ing_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ing_string + """' , '""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
     time_to_insert = st.button('SUBMIT YOUR ORDER')
 
-    #if time_to_insert:
-        #session.sql(my_insert_stmt).collect()
-        #st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 #new section to display smoohyfroot nutritional input
 
