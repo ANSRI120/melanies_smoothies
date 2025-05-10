da36d13ccc08363d92b0a31a094b89b2c31c531c
ANSRI120 <ansri.storage001@gmail.com>
2025-05-11 03:50:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ my_dataframe = session.table ('smoothies.public.fruit_options').select(col('FRUI
 #st.stop
 
 ##convert snowpark dataframe to pandas
-pd_df = my_dataframe.to_pandas();
+pd_df = my_dataframe.to_pandas()
 st.dataframe (pd_df)
 #st.stop()
 ##multiselect
